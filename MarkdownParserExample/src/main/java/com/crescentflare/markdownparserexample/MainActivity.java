a39f384a0e43b181d6981114c045b5d33b18f215
Johan Bos <johan@Johans-MacBook.local>
2016-07-26 22:48:37 +0200

    Added sample for custom styling implementation

--- MarkdownParserExample/src/main/java/com/crescentflare/markdownparserexample/MainActivity.java
+++ MarkdownParserExample/src/main/java/com/crescentflare/markdownparserexample/MainActivity.java
@@ -1,13 +1,24 @@
 package com.crescentflare.markdownparserexample;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
 import android.widget.TextView;
 
 import com.crescentflare.markdownparser.MarkdownConverter;
+import com.crescentflare.markdownparser.helper.AlignedListSpan;
+import com.crescentflare.markdownparser.helper.MarkdownSpanGenerator;
+import com.crescentflare.markdownparsercore.MarkdownTag;
 
 
 /**
@@ -15,13 +26,24 @@ import com.crescentflare.markdownparser.MarkdownConverter;
  */
 public class MainActivity extends AppCompatActivity
 {
-    //Enable/disable conversion through HTML
-    //
-    //HTML doesn't support these markdown features without a custom tag handler:
-    //- Strike through text
-    //- Ordered and unordered lists
+    /**
+     * Enable/disable conversion through HTML
+     *
+     * HTML doesn't support these markdown features without a custom tag handler:
+     * - Strike through text
+     * - Ordered and unordered lists
+     */
     static final boolean TEST_HTML = false;
 
+    /**
+     * Enable/disable custom styling (if HTML is disabled above)
+     */
+    static final boolean TEST_CUSTOM_STYLING = false;
+
+
+    /**
+     * Main sample
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -32,13 +54,103 @@ public class MainActivity extends AppCompatActivity
         final TextView markdownView = (TextView)findViewById(R.id.activity_main_text);
         if (TEST_HTML)
         {
-            String htmlString = MarkdownConverter.toHtmlString(markdownText);
-            markdownView.setText(Html.fromHtml(htmlString), TextView.BufferType.SPANNABLE);
+            testHtml(markdownView, markdownText);
         }
         else
         {
-            markdownView.setText(MarkdownConverter.toSpannable(markdownText), TextView.BufferType.SPANNABLE);
+            if (TEST_CUSTOM_STYLING)
+            {
+                testCustomStyling(markdownView, markdownText);
+            }
+            else
+            {
+                testDefaultSpanConversion(markdownView, markdownText);
+            }
         }
         markdownView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    /**
+     * Tests
+     */
+    private void testHtml(final TextView markdownView, final String markdownText)
+    {
+        String htmlString = MarkdownConverter.toHtmlString(markdownText);
+        markdownView.setText(Html.fromHtml(htmlString), TextView.BufferType.SPANNABLE);
+    }
+
+    private void testCustomStyling(final TextView markdownView, final String markdownText)
+    {
+        markdownView.setText(MarkdownConverter.toSpannable(markdownText, new MarkdownSpanGenerator()
+        {
+            @Override
+            public void applySpan(SpannableStringBuilder builder, MarkdownTag.Type type, int weight, int start, int end, String extra)
+            {
+                switch (type)
+                {
+                    case Paragraph:
+                        builder.setSpan(new RelativeSizeSpan(weight * 0.5f), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        break;
+                    case Header:
+                        builder.setSpan(new RelativeSizeSpan(2 - weight * 0.15f), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        break;
+                    case OrderedList:
+                    case UnorderedList:
+                        builder.setSpan(new AlignedListSpan(extra, 20 + (weight - 1) * 10, 8), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        break;
+                    case TextStyle:
+                    {
+                        int textStyle = Typeface.NORMAL;
+                        switch (weight)
+                        {
+                            case 1:
+                                textStyle = Typeface.ITALIC;
+                                break;
+                            case 2:
+                                textStyle = Typeface.BOLD;
+                                break;
+                            case 3:
+                                textStyle = Typeface.BOLD_ITALIC;
+                                break;
+                        }
+                        builder.setSpan(new StyleSpan(textStyle), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        break;
+                    }
+                    case AlternativeTextStyle:
+                        builder.setSpan(new StrikethroughSpan(), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        break;
+                    case Link:
+                        builder.setSpan(new URLSpan(extra), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        break;
+                }
+            }
+
+            @Override
+            public String getListToken(MarkdownTag.Type type, int weight, int index)
+            {
+                String token = "";
+                if (type == MarkdownTag.Type.OrderedList)
+                {
+                    for (int i = 0; i < index; i++)
+                    {
+                        token += "i";
+                    }
+                    token += ".";
+                }
+                else
+                {
+                    for (int i = 0; i < weight; i++)
+                    {
+                        token += ">";
+                    }
+                }
+                return token;
+            }
+        }), TextView.BufferType.SPANNABLE);
+    }
+
+    private void testDefaultSpanConversion(final TextView markdownView, final String markdownText)
+    {
+        markdownView.setText(MarkdownConverter.toSpannable(markdownText), TextView.BufferType.SPANNABLE);
     }
 }
